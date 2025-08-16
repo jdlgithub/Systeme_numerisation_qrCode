5c855a00d7096050c4bc837041efd26549349a36
jdlgithub <jaudelekoum39@gmail.com>
2025-08-16 07:09:05 +0100

    Fix mistake in local config bd

--- config_local.py
+++ config_local.py
@@ -5,8 +5,8 @@ class Config:
     
     # Configuration MySQL pour développement local
     DB_HOST = 'localhost'
-    DB_USER = 'Jdl'  # Remplacez par votre nom d'utilisateur MySQL
-    DB_PASSWORD = 'Minecraft77jdl@.'  # Remplacez par votre mot de passe MySQL
+    DB_USER = 'root'  # Remplacez par votre nom d'utilisateur MySQL
+    DB_PASSWORD = 'Your password session'  # Remplacez par votre mot de passe MySQL
     DB_NAME = 'qr_archives'
     DB_PORT = 3306
     
